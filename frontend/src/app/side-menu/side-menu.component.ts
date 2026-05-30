6f0a5aa87d44254329683ee70aab3bcae6666b05
Dave <dtimmons2747@hotmail.com>
2026-05-29 23:26:10 -0400

    Restrict mobile users to leaderboard and scorecard pages only

--- frontend/src/app/side-menu/side-menu.component.ts
+++ frontend/src/app/side-menu/side-menu.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss'
 })
 export class SideMenuComponent {
+  isMobile = signal(false);
 
+  constructor() {
+    this.checkIsMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkIsMobile();
+  }
+
+  private checkIsMobile() {
+    this.isMobile.set(window.innerWidth <= 768);
+  }
 }

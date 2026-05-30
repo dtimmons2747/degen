d9c3f9af52bb6ebb5f919d7ad2a5a539d5e1ab0d
Dave <dtimmons2747@hotmail.com>
2026-05-29 23:04:55 -0400

    Add mobile-responsive design - hamburger menu for tablets/phones, desktop layout unchanged

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, SideMenuComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Degen';
+  mobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(val => !val);
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
 }
